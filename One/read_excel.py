93378d97fdab03181955fab349c97054d9be4d0c
Martin Akolo <chiteri@geek.co.ke>
2012-02-11 11:57:18 +0300

    Initial commit for the exercises.

---
+++ One/read_excel.py
@@ -0,0 +1,18 @@
+import csv 
+import matplotlib.pyplot as plt 
+
+spamReader = csv.reader(open('data/data_file_1.dat'), delimiter=' ', quotechar='|') 
+
+xls_list = [] 
+
+for row in spamReader:
+    xls_list.append (row)
+
+plt.plot( xls_list )
+
+plt.xlabel('Cells in Worksheet')
+plt.ylabel('Values of excel column')
+plt.title('Plot of First CMS data')
+plt.grid(True)
+
+plt.show()
