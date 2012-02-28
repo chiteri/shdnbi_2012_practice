83e67d40ab68dbd175b312e2168890ce4405076d
Martin Akolo <chiteri@geek.co.ke>
2012-02-28 06:24:27 +0300

    Made the program more flexible by using extra command line arguments as inputs.

--- Assignment_02/plot_data.py
+++ Assignment_02/plot_data.py
@@ -1,13 +1,10 @@
 import matplotlib.pyplot as plt 
 import numpy as np 
 from matplotlib.ticker import FormatStrFormatter
+import argparse # Assist in the parsing of command line arguments 
 
-def draw_plots( data_file): 
+def draw_plots( data_file, main_particle_mass, range_minimum, range_maximum ): 
     data_list = [] # A list data type to hold data from an input file 
-    # data_list1 = [] # A list data type to hold data from second input file 
-
-    j_psi_percentile = (3.1 / 4.0) # The percentage of mass of the J/psi particles plotted 
-    upsilon_percentile = (4.0 - 3.1) / 4.0# The percentage of mass of the J/psi particles plotted 
 
     # Open the first file and read its content
     with open(data_file, 'r' ) as file:
@@ -16,25 +13,27 @@ def draw_plots( data_file):
 
     data_list.sort() # Order the items in the list 
 
-
     # Draw a hisotgram with their data values of the first file
     fig, ax = plt.subplots(1,1,1)
 
-    counts, bins, patches = plt.hist(data_list, 50, normed=1, facecolor='yellow', edgecolor='gray', alpha=0.75, range=(2.0, 4.0)) 
+    counts, bins, patches = plt.hist(data_list, 50, normed=1, facecolor='yellow', edgecolor='gray', alpha=0.75, range=(range_minimum, range_minimum)) 
 
     # Set the ticks to be at the edges of the bins.
     ax.set_xticks(bins)
 
     # Set the xaxis's tick labels to be formatted with 1 decimal place...
-    ax.xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
+    ax.xaxis.set_major_formatter(FormatStrFormatter('%0.1f')) 
+	
+    main_particle_percentile = (main_particle_mass / range_maximum) # The percentage of mass of the J/psi particles plotted 
+    other_particle_percentile = (range_maximum - main_particle_mass) / range_maximum # The percentage of mass of the J/psi particles plotted 
 
     # Change the colors of bars at the edges...
-    j_psi_particles, upsilon_particles = np.percentile( data_list, [ j_psi_percentile, upsilon_percentile ] )
+    main_particles, other_particles = np.percentile( data_list, [ main_particle_percentile, other_particle_percentile ] )
 
     for patch, rightside, leftside in zip(patches, bins[1:], bins[:-1]):
-        if rightside < j_psi_particles:
+        if rightside < main_particles:
             patch.set_facecolor('green')
-        elif leftside > upsilon_particles:
+        elif leftside > other_particles:
             patch.set_facecolor('red')
 		
     # Label the raw counts and the percentages below the x-axis...
@@ -65,5 +64,11 @@ def draw_plots( data_file):
     plt.show()
 
 if __name__ == "__main__": 
-    import sys 
-    draw_plots (str(sys.argv[1]))
+    parser = argparse.ArgumentParser(description='Look for different particles, J/Psi, Upsilon and the z-Boson in LHC data.')
+    parser.add_argument('-f', '--file', type=str, nargs='+', help='The paths to the file(s) holding the input data.')
+    parser.add_argument('-p', '--particle-mass', dest='particle_mass', type=float, nargs='+', help='A floating point number to hold the mass (GeV / c^2) of the particle to search for.')
+    parser.add_argument('-r', '--range', nargs='+', help='Range of masses within which to plot the histograms.')
+
+    args = parser.parse_args() 
+    
+    draw_plots (args.file[0], float(args.particle_mass[0]), float(args.range[0]), float(args.range[1]))
