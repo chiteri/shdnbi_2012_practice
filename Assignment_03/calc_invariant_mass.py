3ca5ababe38f8b2d108e36486d7bc5b3637ee96c
Martin Akolo <chiteri@geek.co.ke>
2012-04-19 05:40:52 +0300

    Program now loops correctly and displays data values on screen.

--- Assignment_03/calc_invariant_mass.py
+++ Assignment_03/calc_invariant_mass.py
@@ -1,7 +1,11 @@
 import argparse
 
 def determine_mass(data_file): 
+    daughter_particles = 0 # How many particles were in event 
+	
     # Open the first file and read its content 
+    # Format the headings for the data before display 
+    print repr('Energy').ljust(7), repr('X-coord').ljust(7), repr('Y-coord').ljust(7), repr('Z-coord').ljust(7), repr('Charge').ljust(7)
     with open(data_file, 'r' ) as file: 	
         for particle_details in file: # Read the data obtained line by line 
             values = particle_details.split() # The first item can tell us if we are looking at daughter particles or four momentum values
@@ -10,19 +14,22 @@ def determine_mass(data_file):
                 daughter_particles = int(values[0]) # The first line represents the number of particles to expect in subsequent events
 	   
             elif len( values ) > 1: 
-                # particle in range(1, daughter_particles): # Loop through the number of particles detected in the decay 
-                energy, px, py, pz, charge = values[0], values[1], values[2], values[3],  values[4] # Unpack the list. Get the four momentum data 
-                print energy, px, py, pz, charge 
-                # mass = invariant_mass(energy, px, py, pz, charge)
+                for n in range (0, daughter_particles): 
+                    # Unpack the list. Get the four momentum data 
+                    energy, px, py, pz, charge = float(values[0]), float(values[1]), float(values[2]), float(values[3]),  int(values[4]) 
+                    # mass = invariant_mass( energy, px, py, pz ) 					
+                    print repr(energy).rjust(8), repr(px).rjust(8), repr(py).rjust(8), repr(pz).rjust(8), repr(charge).rjust(8) 
+                    break # Temporarily stop execution, force the program to go to the next loop iteration  
 			
 ################################################################################
 # Calculate the invariant mass according to the theory of special relativity
 ################################################################################
 def invariant_mass(E, px, py, pz):
+    # mass  = (E[0]+E[1])**2
+    # mass -= (px[0]+px[1])**2
+    # mass -= (py[0]+py[1])**2
+    # mass -= (pz[0]+pz[1])**2  
     mass  = (E[0]+E[1])**2
-    mass -= (px[0]+px[1])**2
-    mass -= (py[0]+py[1])**2
-    mass -= (pz[0]+pz[1])**2
 
     return np.sqrt(mass)
 			
